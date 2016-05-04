76b6e07bbb1d74857f9db7caab15bd3d22e0461c
Lilia Hristova <lilia.hristova@softwaregroup-bg.com>
2016-05-04 17:49:39 +0300

    Added new repository api
    Added some classes and 1 controller for Orde

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using enti_api.Models;
+
+namespace enti_api.Controllers
+{
+    public class OrderController : ApiController
+    {
+        // GET: api/Order
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Order/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Order
+        //Content-Type: application/json
+        //{"shoppingCart": [{"quantity": "2", "item": {"title":"Test title"}}], "customer": {"name":"Lily", "email":"test@abv.bg", "phone": "9847", "address": "asdf", "message":"msg msg"}}
+        public void Post(Order order)
+        {
+
+        }
+
+        //// PUT: api/Order/5
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE: api/Order/5
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace enti_api.Models
+{
+    public class Order
+    {
+        public List<OrderItem> shoppingCart { get; set; }
+        public Customer customer { get; set; }
+    }
+    
+    public class OrderItem
+    {
+        public ShoppingItem item { get; set; }
+        public int quantity { get; set; }
+    }
+}
