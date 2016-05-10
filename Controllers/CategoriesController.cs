c8fae98297fd66246dfdade207392e9aef545d35
Lilia Hristova <lilia.hristova@softwaregroup-bg.com>
2016-05-10 11:45:31 +0300

    Added new models and controllers
    Added new models and controllers

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using enti_api.Models;
+
+namespace enti_api.Controllers
+{
+    public class CategoriesController : ApiController
+    {
+
+        // GET: api/Categories/5
+        public List<Models.Category> Get()
+        {
+            var cat = new Models.Category
+            {
+                id = 1,
+                description = "asdf",
+                imageSrc = "1.jpg",
+                itemsCount = 5,
+                name = "test",
+                src = "shop/soil",
+                systemName = "soil"
+            };
+            var list = new List<Models.Category>();
+            list.Add(cat);
+            return list;
+        }
+    }
+}
---
+++ Controllers/ShoppingItemsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace enti_api.Controllers
+{
+    public class ShoppingItemsController : ApiController
+    {
+       
+        // GET: api/ShoppingItems/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/ShoppingItems
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/ShoppingItems/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/ShoppingItems/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
